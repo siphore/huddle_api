474e6033711ff83237c73fa1603bae675654fbc7
Michael <michael.cheneval@logixcube.ch>
2025-03-13 11:01:13 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+import express from "express";
+import createError from "http-errors";
+import logger from "morgan";
+import mongoose from "mongoose";
+
+import indexRouter from "./routes/index.js";
+import usersRouter from "./routes/users.js";
+
+mongoose.connect(
+  process.env.DATABASE_URL || "mongodb://localhost/huddle-api"
+);
+
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // Send the error status
+  res.status(err.status || 500);
+  res.send(err.message);
+});
+
+export default app;
