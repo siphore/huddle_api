46cfde777e91b1227658b566bd02f5a78ef3d9fa
Michael <michael.cheneval@logixcube.ch>
2025-03-13 11:21:50 +0100

    Connect to mongoose DB

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ import express from "express";
 import createError from "http-errors";
 import logger from "morgan";
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 import indexRouter from "./routes/index.js";
 import usersRouter from "./routes/users.js";
 
-mongoose.connect(
-  process.env.DATABASE_URL || "mongodb://localhost/huddle-api"
-);
+mongoose.connect(process.env.DATABASE_URL || "mongodb://localhost/huddle-api");
 
 const app = express();
 
